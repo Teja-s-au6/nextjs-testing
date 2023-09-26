e3f0009f00c4187fa33f977ad48f9be1bacf21f2
Tejas <tejaspvg@gmail.com>
2023-09-26 21:47:06 +0530

    link changed

--- src/app/Components/navbar.tsx
+++ src/app/Components/navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <div>
       <Link href="/">Home</Link>
-      <Link href="/posts?q=type">Posts</Link>
+      <a href="/posts?q=type">Posts</a>
       <Link href="/about">About</Link>
       <Link href="/about/us">About US</Link>
       <Link href="/about/someone">About Someone</Link>
